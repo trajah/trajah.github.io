470e762d027c5c5b4313c45b25caa5cb151cc886
Timothy Rajah <timrajah@gmail.com>
2016-05-27 22:04:06 -0400

    changed picture for Amish Logo

--- mini_magazine/menu.js
+++ mini_magazine/menu.js
@@ -4,15 +4,10 @@ $(function() {
         menu        = $('nav ul');  
         menuHeight  = menu.height();  
   
+    $(menu).hide();
+
     $(pull).on('click', function(e) {  
         e.preventDefault();  
         menu.slideToggle();  
     });  
 });  
-
-$(window).resize(function(){  
-    var w = $(window).width();  
-    if(w > 320 && menu.is(':hidden')) {  
-        menu.removeAttr('style');  
-    }  
-}); 
