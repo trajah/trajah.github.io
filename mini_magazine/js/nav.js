41e5172c7d3597fb65603b1d5849dba15b0b6b78
Timothy Rajah <timrajah@gmail.com>
2016-06-01 01:04:59 -0400

    changed sites

---
+++ mini_magazine/js/nav.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+<script src="http://code.jquery.com/jquery-latest.min.js"
+        type="text/javascript"></script>
+        <script type="text/javascript">
+$(function() {  
+    var pull        = $('#pull');  
+        menu        = $('nav ul');  
+        menuHeight  = menu.height();  
+  
+    $(pull).on('click', function(e) {  
+        e.preventDefault();  
+        menu.slideToggle();  
+    });  
+});  
+
+$(window).resize(function(){  
+    var w = $(window).width();  
+    if(w > 320 && menu.is(':hidden')) {  
+        menu.removeAttr('style');  
+    }  
+}); 
+
+        </script>
